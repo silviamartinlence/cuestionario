c373e42aa046429f58763e42c705c0da52345640
Elena Bravo <hey@elenabravo.com>
2020-06-12 16:43:09 +0200

    add probability to generate urls

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ var link = document.getElementById('linkCuestionario');
 
 // Create url array
 var urls = [
-    'https://forms.gle/eLmWVDVB4ZegHjd19',
-    'https://forms.gle/fk6j7D4KRCAGsvJm7'
+    'https://forms.gle/eLmWVDVB4ZegHjd19', // 40% chance to select
+    'https://forms.gle/fk6j7D4KRCAGsvJm7' // 60% chance to select
 ];
 
+//Chances of being selected
+var chances = [40, 60];
+
 // Get random url from array
-var randomUrl = urls[Math.floor(Math.random() * urls.length)];
+var sum = chances.reduce((acc, el) => acc + el, 0);
+var acc = 0;
+chances = chances.map(el => (acc = el + acc));
+var rand = Math.random() * sum;
+var randomUrl = urls[chances.filter(el => el <= rand).length];
 
 // Set link href to random url
 link.href = randomUrl;
-
