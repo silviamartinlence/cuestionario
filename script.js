76021d6c9a568bfeb31eba690bf406ad3e039de9
Elena Bravo <hey@elenabravo.com>
2020-06-12 18:22:43 +0200

    increase second url select chance

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // Select link element
-var link = document.getElementById('linkCuestionario');
+var link = document.getElementById('linkCuestionario')
 
 // Create url array
 var urls = [
-    'https://forms.gle/eLmWVDVB4ZegHjd19', // 40% chance to select
-    'https://forms.gle/fk6j7D4KRCAGsvJm7' // 60% chance to select
-];
+  'https://forms.gle/eLmWVDVB4ZegHjd19', // 40% chance to select
+  'https://forms.gle/fk6j7D4KRCAGsvJm7', // 60% chance to select
+]
 
 //Chances of being selected
-var chances = [40, 60];
+var chances = [30, 70]
 
 // Get random url from array
-var sum = chances.reduce((acc, el) => acc + el, 0);
-var acc = 0;
-chances = chances.map(el => (acc = el + acc));
-var rand = Math.random() * sum;
-var randomUrl = urls[chances.filter(el => el <= rand).length];
+var sum = chances.reduce((acc, el) => acc + el, 0)
+var acc = 0
+chances = chances.map((el) => (acc = el + acc))
+var rand = Math.random() * sum
+var randomUrl = urls[chances.filter((el) => el <= rand).length]
 
 // Set link href to random url
-link.href = randomUrl;
+link.href = randomUrl
